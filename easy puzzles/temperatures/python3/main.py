1de8dac87f9703db2f967abc8f09e74e988cdcf1
thomdaro <31260388+thomdaro@users.noreply.github.com>
2020-03-03 13:58:54 -0500

    Update main.py for "Temperatures"
    Fix logic to make use of all input; exit program before some initializations in the case of n = 0.

--- easy puzzles/temperatures/python3/main.py
+++ easy puzzles/temperatures/python3/main.py
@@ -3,17 +3,18 @@ import math
 
 
 n = int(input())
+
+if n == 0:
+    print("0")
+    sys.exit()
+    
 # Set threshold to 1 above the max
 closest = 5527
 
 for i in input().split():
     t = int(i)
     # Compare absolute values, change to positive if necessary
-    if abs(t) < abs(closest) or (t == abs(closest) and t > 0):
+    if abs(t) < abs(closest) or t == abs(closest):
         closest = t
-        
-# If no input was received, output 0
-if closest == 5527:
-    closest = 0
 
 print(str(closest))
