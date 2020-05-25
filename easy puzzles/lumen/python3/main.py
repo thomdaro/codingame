39e516e1f08da4b474867a7d51deedd678619d33
thomdaro <31260388+thomdaro@users.noreply.github.com>
2020-05-24 20:19:14 -0400

    Update main.py for "Lumen"
    Remove unnecessary debug printing.

--- easy puzzles/lumen/python3/main.py
+++ easy puzzles/lumen/python3/main.py
@@ -6,8 +6,6 @@ l = int(input())
 room = [[] for _ in range(n)]
 candle_psns = []
 
-print("layout: ", file=sys.stderr)
-
 for i in range(n):
     line = input()
     for j in range(n*2-1):
@@ -15,15 +13,10 @@ for i in range(n):
         if char != ' ':
             if char == "X":
                 room[i].append(0)
-                print("X", end=" ", file=sys.stderr)
             elif char == "C":
                 room[i].append(l)
                 candle_psns.append((i, j // 2))
-                print("C", end=" ",file=sys.stderr)
-    print("\b", file=sys.stderr)
-    
-print("\nbase light lvl: " + str(l), file=sys.stderr)
-
+   
 for pos in candle_psns:
     for i in range(1,l):
         for j in range(-i,i+1):
@@ -33,12 +26,6 @@ for pos in candle_psns:
                         room[pos[0]+j][pos[1]+k] += l-i
                 except IndexError:
                     pass
-   
-print("\nlightmap: ", file=sys.stderr)
-for line in room:
-    for char in line:
-        print(char, file=sys.stderr, end=" ")
-    print("\b", file=sys.stderr)
 
 res = 0
 for line in room:
@@ -46,5 +33,4 @@ for line in room:
         if char == 0:
             res += 1
 
-print(file=sys.stderr)
 print(str(res))
